705e35a1fe608888728a46c98597e9a1801f1695
Matsuel <matsuel0308@gmail.com>
2025-04-04 11:14:44 +0200

    Ajout LineChart

--- src/main/java/com/financial/demojavagradle/controller/DashboardController.java
+++ src/main/java/com/financial/demojavagradle/controller/DashboardController.java
@@ -3,7 +3,11 @@ package com.financial.demojavagradle.controller;
 import com.financial.demojavagradle.Line;
 import com.financial.demojavagradle.db.ExpenseDAO;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
+
+import java.util.List;
 
 public class DashboardController {
 
@@ -11,8 +15,12 @@ public class DashboardController {
     private PieChart expensePieChart;
 
     @FXML
+    private LineChart<String, Number> expenseLineChart;
+
+    @FXML
     private void initialize() {
         loadPieChartData();
+        loadLineChartData();
     }
 
     private void loadPieChartData() {
@@ -20,7 +28,6 @@ public class DashboardController {
 
         if (recentExpenses.getTotal() == null) return;
 
-        System.out.println(recentExpenses);
         expensePieChart.getData().add(new PieChart.Data("Logement", recentExpenses.getLogement()));
         expensePieChart.getData().add(new PieChart.Data("Nourriture", recentExpenses.getNourriture()));
         expensePieChart.getData().add(new PieChart.Data("Sorties", recentExpenses.getSorties()));
@@ -29,10 +36,47 @@ public class DashboardController {
         expensePieChart.getData().add(new PieChart.Data("Impots", recentExpenses.getImpots()));
         expensePieChart.getData().add(new PieChart.Data("Autres", recentExpenses.getAutres()));
 
-        expensePieChart.setTitle("Dépenses du mois dernier" + recentExpenses.getPeriod());
+        expensePieChart.setTitle("Dépenses du mois dernier " + recentExpenses.getPeriod());
 
         expensePieChart.setLegendVisible(true);
 
         expensePieChart.setLabelsVisible(true);
+    }
+
+    private void loadLineChartData() {
+        List<Line> expenses = ExpenseDAO.getExpense();
+        String[] categories = {"Housing", "Food", "Exits", "Transport", "Travel", "Taxes", "Other"};
+
+        for (String category : categories) {
+            XYChart.Series<String, Number> series = new XYChart.Series<>();
+            series.setName(category);
+            for (Line line : expenses) {
+                switch (category) {
+                    case "Housing":
+                        series.getData().add(new XYChart.Data<>(line.getPeriod(), line.getLogement()));
+                        break;
+                    case "Food":
+                        series.getData().add(new XYChart.Data<>(line.getPeriod(), line.getNourriture()));
+                        break;
+                    case "Exits":
+                        series.getData().add(new XYChart.Data<>(line.getPeriod(), line.getSorties()));
+                        break;
+                    case "Transport":
+                        series.getData().add(new XYChart.Data<>(line.getPeriod(), line.getTransports()));
+                        break;
+                    case "Travel":
+                        series.getData().add(new XYChart.Data<>(line.getPeriod(), line.getVoyages()));
+                        break;
+                    case "Taxes":
+                        series.getData().add(new XYChart.Data<>(line.getPeriod(), line.getImpots()));
+                        break;
+                    case "Other":
+                        series.getData().add(new XYChart.Data<>(line.getPeriod(), line.getAutres()));
+                        break;
+                }
+            }
+            expenseLineChart.getData().add(series);
+        }
+        expenseLineChart.setTitle("Expenses Over Time");
     }
 }
