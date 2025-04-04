452cb63f0f20fef7a0a9bc1ca43d153aeffb8a2b
Matsuel <matsuel0308@gmail.com>
2025-04-04 10:10:18 +0200

    Switch entre les 2 pages

--- src/main/java/com/financial/demojavagradle/controller/HeaderController.java
+++ src/main/java/com/financial/demojavagradle/controller/HeaderController.java
@@ -1,7 +1,6 @@
 package com.financial.demojavagradle.controller;
 
 import com.financial.demojavagradle.HelloApplication;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import java.io.IOException;
